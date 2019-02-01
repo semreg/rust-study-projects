c3201ebc25e834c726795bf6b7e880f5f383b8c7
semreg <vchabaniuk@outlook.com>
2019-02-01 21:32:53 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# rust-guess-game
-Learning Rust Project
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let mut counter: i32 = 0;
+
+    println!("Guess the number!\n");
+
+    let random_number = rand::thread_rng()
+        .gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("\nYou guessed: {}", guess);
+
+        match guess.cmp(&random_number) {
+            Ordering::Less => {
+                counter += 1;
+                println!("Too small!\n");
+            },
+
+            Ordering::Greater => {
+                counter += 1;
+                println!("Too big!\n");
+            },
+
+            Ordering::Equal => {
+                counter += 1;
+                println!("Congrats! You win!\nIt took {} guessing attempt(s)!", counter);
+                break;
+            }
+        }
+    }
+}
